c05f5fc2999a0ba6014092f483962fbdf76f2328
Arjunsing1997 <acchur1997@gmail.com>
2021-05-04 18:57:19 +0530

    UC4-Reflection-DefaultConstructor

--- MoodAnalyser-MSTest/MoodAnalyser-Test1/UnitTest1.cs
+++ MoodAnalyser-MSTest/MoodAnalyser-Test1/UnitTest1.cs
@@ -85,5 +85,60 @@ namespace MoodAnalyser_Test1
             }
 
         }
+
+        [TestMethod]
+        public void GivenMoodAnalyseClass_ShouldReturn_MoodAnalyserObject()
+        {
+            string message = null;
+            object expected = new MoodAnalyser(message);
+            MoodAnalyserFactor factory = new MoodAnalyserFactor();
+            object obj = factory.CreateMoodAnalyzerObject("MoodAnalyzerProblem.MoodAnalyzer", "MoodAnalyzer");
+            expected.Equals(obj);
+        }
+
+        /* TC 4.2:- Given Class Name When Improper Should Throw MoodAnalysisException
+                    - To pass this test case pass wrong class name catch Exception and throw 
+                    Exception indicating No Such Class Error.
+         */
+        [TestMethod]
+        public void MoodAnalyseClass_GivenWrongClassName_ShouldReturn_NOClassException()
+        {
+            string expected = "Class not found";
+            try
+            {
+                string message = null;
+                object moodAnalyser = new MoodAnalyser(message);
+                MoodAnalyserFactor factory = new MoodAnalyserFactor();
+                object obj = factory.CreateMoodAnalyzerObject("MoodAnalyzerProblem.MoodAnalyzer", "MoodAnalyzer");
+                moodAnalyser.Equals(obj);
+            }
+            catch (MoodAnalyserException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+
+        }
+        /*TC 4.3:- Given Class When Constructor Not Proper Should Throw MoodAnalysisException.
+                  - To pass this Test Case pass wrong Constructor parameter, catch the Exception and throw indicating No 
+                   Such Method Error. 
+        */
+        [TestMethod]
+        public void MoodAnalyseClass_GivenWrongConstructorName_ShouldReturn_NoConstructorException()
+        {
+            string expected = "Constructor not found";
+            try
+            {
+                string message = null;
+                object moodAnalyser = new MoodAnalyser(message);
+                MoodAnalyserFactor factory = new MoodAnalyserFactor();
+                object obj = factory.CreateMoodAnalyzerObject("MoodAnalyzerProblem.MoodAnalyzer", "MoodAnalyzer");
+                moodAnalyser.Equals(obj);
+            }
+            catch (MoodAnalyserException ex)
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+
+        }
     }
 }
